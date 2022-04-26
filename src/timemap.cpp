458e5e0082cf0d7a9ea7162bda271fc289a872f5
Herbert <yhb198854@163.com>
2022-04-26 19:14:47 +0800

    Revert "extend timemap structure"
    This reverts commit 596007ae3da1478687171e56f5adcb0d5bbe49b9.

--- src/timemap.cpp
+++ src/timemap.cpp
@@ -17,11 +17,6 @@
 #include "measure.h"
 #include "note.h"
 #include "rest.h"
-#include "staff.h"
-#include "system.h"
-#include "page.h"
-#include "elementpart.h"
-#include "artic.h"
 #include "vrv.h"
 
 namespace vrv {
@@ -70,20 +65,6 @@ void Timemap::AddEntry(Object *object, GenerateTimemapParams *params)
         if (m_map.count(realTimeStart) == 0) {
             m_map[realTimeStart] = emptyEntry;
         }
-
-        auto staff = dynamic_cast<Staff*>(object->GetFirstAncestor(STAFF));
-        int staffNo = staff ? staff->GetN() : 0;
-        auto beam = dynamic_cast<Beam *>(object->GetFirstAncestor(vrv::BEAM));
-        auto accid = dynamic_cast<Accid *>(object->FindDescendantByType(vrv::ACCID));
-        auto atric = dynamic_cast<Artic *>(object->FindDescendantByType(vrv::ARTIC));
-        auto dots = dynamic_cast<Dots *>(object->FindDescendantByType(vrv::DOTS));
-        auto stem = dynamic_cast<Stem *>(object->FindDescendantByType(vrv::STEM));
-        auto flag = stem ? dynamic_cast<Flag *>(stem->FindDescendantByType(vrv::FLAG)) : nullptr;
-        auto chord = dynamic_cast<Chord*>(object->GetFirstAncestor(CHORD));
-        auto chordStem = chord ? dynamic_cast<Stem *>(chord->FindDescendantByType(vrv::STEM)) : nullptr;
-        auto chordFlag = chord ? dynamic_cast<Flag *>(chord->FindDescendantByType(vrv::FLAG)) : nullptr;
-        auto chordDots = chord ? dynamic_cast<Dots *>(chord->FindDescendantByType(vrv::DOTS)) : nullptr;
-
         TimemapEntry *startEntry = &m_map.at(realTimeStart);
 
         // Should check if value for realTimeStart already exists and if so, then
@@ -91,58 +72,8 @@ void Timemap::AddEntry(Object *object, GenerateTimemapParams *params)
         startEntry->qstamp = scoreTimeStart;
 
         // Store the element ID in list to turn on at given time - note or rest
-        if (!isRest) {
-            Note* note = dynamic_cast<Note*>(object);
-            auto pitch = note->GetMIDIPitch();
-            auto uuid = object->GetUuid();
-            std::vector<std::string> elements;
-            elements.emplace_back(uuid);
-            if (beam) {
-                elements.emplace_back(beam->GetUuid());
-            }
-            if (accid) {
-                elements.emplace_back(accid->GetUuid());
-            }
-            if (atric) {
-                elements.emplace_back(atric->GetUuid());
-            }
-            if (dots){
-                elements.emplace_back(dots->GetUuid());
-            }
-            if (stem) {
-                elements.emplace_back(stem->GetUuid());
-            }
-            if (flag) {
-                elements.emplace_back(flag->GetUuid());
-            }
-            if(chordFlag){
-                elements.emplace_back(chordFlag->GetUuid());
-            }
-            if(chordStem){
-                elements.emplace_back(chordStem->GetUuid());
-            }
-            if(chordDots){
-                elements.emplace_back(chordDots->GetUuid());
-            }
-            startEntry->notesOn.emplace(pitch, std::make_pair(staffNo, elements));
-        }
-        if (isRest) {
-            startEntry->restsOn.push_back(object->GetUuid());
-        }
-
-        Measure* measure = dynamic_cast<Measure*>(object->GetFirstAncestor(MEASURE));
-        if (measure) {
-            startEntry->measureNo = std::stoi(measure->GetN());
-        }
-
-        System* system = dynamic_cast<System*>(object->GetFirstAncestor(SYSTEM));
-        if (system) {
-            startEntry->systemNo = system->GetSystemIdx();
-        }
-        Page* page = dynamic_cast<Page*>(object->GetFirstAncestor(PAGE));
-        if (page) {
-            startEntry->pageNo = page->GetPageIdx();
-        }
+        if (!isRest) startEntry->notesOn.push_back(object->GetUuid());
+        if (isRest) startEntry->restsOn.push_back(object->GetUuid());
 
         // Also add the tempo the
         startEntry->tempo = params->m_currentTempo;
@@ -159,54 +90,8 @@ void Timemap::AddEntry(Object *object, GenerateTimemapParams *params)
         endEntry->qstamp = scoreTimeEnd;
 
         // Store the element ID in list to turn off at given time - notes or rest
-        if (!isRest) {
-            Note* note = dynamic_cast<Note*>(object);
-            auto pitch = note->GetMIDIPitch();
-            auto uuid = object->GetUuid();
-            std::vector<std::string> elements;
-            elements.emplace_back(uuid);
-            if (beam) {
-                elements.emplace_back(beam->GetUuid());
-            }
-            if (accid) {
-                elements.emplace_back(accid->GetUuid());
-            }
-            if (atric) {
-                elements.emplace_back(atric->GetUuid());
-            }
-            if (dots){
-                elements.emplace_back(dots->GetUuid());
-            }
-            if (stem) {
-                elements.emplace_back(stem->GetUuid());
-            }
-            if (flag) {
-                elements.emplace_back(flag->GetUuid());
-            }
-            if(chordFlag){
-                elements.emplace_back(chordFlag->GetUuid());
-            }
-            if(chordStem){
-                elements.emplace_back(chordStem->GetUuid());
-            }
-            if(chordDots){
-                elements.emplace_back(chordDots->GetUuid());
-            }
-            endEntry->notesOff.emplace(pitch, std::make_pair(staffNo, elements));
-        }
-        if (isRest) {
-            endEntry->restsOff.push_back(object->GetUuid());
-        }
-
-        if (measure) {
-            endEntry->measureNo = std::stoi(measure->GetN());
-        }
-        if (system) {
-            endEntry->systemNo = system->GetSystemIdx();
-        }
-        if (page) {
-            endEntry->pageNo = page->GetPageIdx();
-        }
+        if (!isRest) endEntry->notesOff.push_back(object->GetUuid());
+        if (isRest) endEntry->restsOff.push_back(object->GetUuid());
     }
     else if (object->Is(MEASURE)) {
 
@@ -228,16 +113,6 @@ void Timemap::AddEntry(Object *object, GenerateTimemapParams *params)
 
         // Add the measureOn
         startEntry->measureOn = measure->GetUuid();
-        startEntry->measureNo = std::stoi(measure->GetN());
-
-        System* system = dynamic_cast<System*>(object->GetFirstAncestor(SYSTEM));
-        if (system) {
-            startEntry->systemNo = system->GetSystemIdx();
-        }
-        Page* page = dynamic_cast<Page*>(object->GetFirstAncestor(PAGE));
-        if (page) {
-            startEntry->pageNo = page->GetPageIdx();
-        }
     }
 }
 
@@ -256,36 +131,12 @@ void Timemap::ToJson(std::string &output, bool includeRests, bool includeMeasure
         // on / off
         if (!entry.notesOn.empty()) {
             jsonxx::Array notesOn;
-            for (auto& [pitch, pitchEntry] : entry.notesOn) {
-                jsonxx::Object item;
-                item << "pitch" << pitch;
-
-                item << "staff" << pitchEntry.first;
-
-                jsonxx::Array ids;
-                for (auto& id : pitchEntry.second) {
-                    ids << id;
-                }
-                item << "ids" << ids;
-                notesOn << item;
-            }
+            for (auto note : entry.notesOn) notesOn << note;
             o << "on" << notesOn;
         }
         if (!entry.notesOff.empty()) {
             jsonxx::Array notesOff;
-            for (auto& [pitch, pitchEntry] : entry.notesOff) {
-                jsonxx::Object item;
-                item << "pitch" << pitch;
-
-                item << "staff" << pitchEntry.first;
-
-                jsonxx::Array ids;
-                for (auto& id : pitchEntry.second) {
-                    ids << id;
-                }
-                item << "ids" << ids;
-                notesOff << item;
-            }
+            for (auto note : entry.notesOff) notesOff << note;
             o << "off" << notesOff;
         }
 
@@ -293,12 +144,12 @@ void Timemap::ToJson(std::string &output, bool includeRests, bool includeMeasure
         if (includeRests) {
             if (!entry.restsOn.empty()) {
                 jsonxx::Array restsOn;
-                for (auto& rest : entry.restsOn) restsOn << rest;
+                for (auto rest : entry.restsOn) restsOn << rest;
                 o << "restsOn" << restsOn;
             }
             if (!entry.restsOff.empty()) {
                 jsonxx::Array restsOff;
-                for (auto& rest : entry.restsOff) restsOff << rest;
+                for (auto rest : entry.restsOff) restsOff << rest;
                 o << "restsOff" << restsOff;
             }
         }
@@ -316,9 +167,6 @@ void Timemap::ToJson(std::string &output, bool includeRests, bool includeMeasure
         if (includeMeasures && !entry.measureOn.empty()) {
             o << "measureOn" << entry.measureOn;
         }
-        o << "measureNo" << entry.measureNo;
-        o << "systemNo" << entry.systemNo;
-        o << "pageNo" << entry.pageNo;
 
         timemap << o;
     }
